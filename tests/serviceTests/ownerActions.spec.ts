ce931b39d02401aa99e061fc1484ff5d990d29c2
Andrew Skubarenko <andrew.skubarenko@gmail.com>
2021-06-07 16:02:32 +0300

    Add tests for some service owner actions

--- tests/serviceTests/ownerActions.spec.ts
+++ tests/serviceTests/ownerActions.spec.ts
@@ -46,4 +46,54 @@ contract('Service | Owner Actions', accounts => {
     const storageAfterActions = await serviceContractInstance.storage();
     expect(storageAfterActions).to.deep.equal(serviceContractStorage);
   });
+
+  describe('Set_owner', () => {
+    it('should change a contract owner if a caller is a current owner', async () => {
+      const result = await serviceContractInstance.set_owner(simpleAccounts[1].pkh);
+      const storageAfterAction = await serviceContractInstance.storage();
+
+      expect(result).to.exist;
+      expect(result.tx).to.exist;
+      expect(storageAfterAction).to.deep.equal({ ...serviceContractStorage, owner: simpleAccounts[1].pkh });
+
+      await expect(serviceContractInstance.set_pause(true))
+        .to.be.rejectedWith(contractErrors.notOwner);
+    });
+  });
+
+  describe('Set_pause', () => {
+    it('should change a contract state if a caller is a current owner', async () => {
+      let result = await serviceContractInstance.set_pause(true);
+      let storageAfterAction = await serviceContractInstance.storage();
+
+      expect(result).to.exist;
+      expect(result.tx).to.exist;
+      expect(storageAfterAction).to.deep.equal({ ...serviceContractStorage, paused: true });
+
+      result = await serviceContractInstance.set_pause(false);
+      storageAfterAction = await serviceContractInstance.storage();
+
+      expect(result).to.exist;
+      expect(result.tx).to.exist;
+      expect(storageAfterAction).to.deep.equal({ ...serviceContractStorage, paused: false });
+    });
+  });
+
+  describe('Set_deleted', () => {
+    it('should change a contract state if a caller is a current owner', async () => {
+      let result = await serviceContractInstance.set_deleted(true);
+      let storageAfterAction = await serviceContractInstance.storage();
+
+      expect(result).to.exist;
+      expect(result.tx).to.exist;
+      expect(storageAfterAction).to.deep.equal({ ...serviceContractStorage, deleted: true });
+
+      result = await serviceContractInstance.set_deleted(false);
+      storageAfterAction = await serviceContractInstance.storage();
+
+      expect(result).to.exist;
+      expect(result.tx).to.exist;
+      expect(storageAfterAction).to.deep.equal({ ...serviceContractStorage, deleted: false });
+    });
+  });
 });
