ad8ec944478430b730eaddb26c001f0616a0b303
Andrew Skubarenko <andrew.skubarenko@gmail.com>
2021-09-14 00:10:23 +0300

    Add a workaround of the key sorting for the taquito package

--- tests/helpers/useLastTezosToolkit.ts
+++ tests/helpers/useLastTezosToolkit.ts
@@ -1,5 +1,7 @@
+import { ParameterSchema } from '@taquito/michelson-encoder';
 import { InMemorySigner } from '@taquito/signer';
 import { TezosToolkit } from '@taquito/taquito';
+import { Prefix, prefix, b58cdecode } from '@taquito/utils';
 
 interface TruffleContractInternal {
   interfaceAdapter: {
@@ -34,3 +36,38 @@ export const useLastTezosToolkit = <TInstance extends Truffle.ContractInstance<u
 
   return [truffleContract, tezosToolkit] as const;
 };
+
+// TODO: Remove this workaround after this PR: https://github.com/ecadlabs/taquito/pull/1044
+const keyTokenPrototype = Object.getPrototypeOf((new ParameterSchema({ prim: 'key', args: [], annots: [] }) as any).root);
+const keyTokenOriginalCompare = keyTokenPrototype.compare;
+
+const publicKeyPrefixLength = 4;
+const keyTokenGetPrefix = (val: string) => val.substring(0, publicKeyPrefixLength);
+const keyTokenGetP256PublicKeyComparableBytes = (p2pk: string) => b58cdecode(p2pk, prefix[Prefix.P2PK]).slice(1);
+
+const keyTokenCompare = (key1: string, key2: string): number => {
+  const keyPrefix1 = keyTokenGetPrefix(key1);
+  const keyPrefix2 = keyTokenGetPrefix(key2);
+
+  if (keyPrefix1 === Prefix.EDPK && keyPrefix2 !== Prefix.EDPK) {
+    return -1;
+  }
+  else if (keyPrefix1 === Prefix.SPPK && keyPrefix2 !== Prefix.SPPK) {
+    return keyPrefix2 === Prefix.EDPK ? 1 : -1;
+  }
+  else if (keyPrefix1 === Prefix.P2PK) {
+    if (keyPrefix2 !== Prefix.P2PK) {
+      return 1;
+    }
+
+    const keyBytes1 = keyTokenGetP256PublicKeyComparableBytes(key1);
+    const keyBytes2 = keyTokenGetP256PublicKeyComparableBytes(key2);
+    return Buffer.compare(keyBytes1, keyBytes2);
+  }
+
+  return keyTokenOriginalCompare.call(this, key1, key2);
+};
+
+keyTokenPrototype.compare = keyTokenCompare;
+keyTokenPrototype.getPrefix = keyTokenGetPrefix;
+keyTokenPrototype.getP256PublicKeyComparableBytes = keyTokenGetP256PublicKeyComparableBytes;
