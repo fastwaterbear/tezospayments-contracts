6388ce24f5b4c2f7df0175ae35aa0f4cec071d07
Andrew Skubarenko <andrew.skubarenko@gmail.com>
2021-10-20 00:00:05 +0300

    Remove source extensions from the build results

--- scripts/buildTestContracts.sh
+++ scripts/buildTestContracts.sh
@@ -16,18 +16,19 @@ rm -rf $BUILD_DIRECTORY
 mkdir -p $BUILD_MICHELSON_DIRECTORY $BUILD_JSON_DIRECTORY
 
 # $1: a contract file name without extension
+# $2: an extension of the contract file
 compile_contract() {
-    echo "Compile contract: $1"
+    echo "Compile contract: $1.$2"
     eval "$ligo compile-contract --michelson-format=text --output-file $BUILD_MICHELSON_DIRECTORY/$1.tz \
-        $CONTRACTS_DIRECTORY/$1 main"
+        $CONTRACTS_DIRECTORY/$1.$2 main"
     eval "$ligo compile-contract --michelson-format=json --output-file $BUILD_JSON_DIRECTORY/$1.json \
-        $CONTRACTS_DIRECTORY/$1 main"
+        $CONTRACTS_DIRECTORY/$1.$2 main"
 }
 
 # Contracts
 echo "Compiling test contracts..."
 
-compile_contract fa1.2.ligo $@
-compile_contract fa2.ligo $@
+compile_contract fa1.2 ligo $@
+compile_contract fa2 ligo $@
 
 echo "All test contracts are compiled!"
